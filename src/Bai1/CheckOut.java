2825a1aca8972dd8818ee0007e8e5bd460897de6
hoangthuyhust <50496960+hoangthuyhust@users.noreply.github.com>
2021-07-24 23:03:38 +0700

    first commit

---
+++ src/Bai1/CheckOut.java
@@ -0,0 +1,28 @@
+package Bai1;
+import java.util.Scanner;
+
+public class CheckOut {
+    public static void main(String[] args) {
+        String discode ="testing";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap vao discount code: ");
+        String code= sc.nextLine();
+        String app=code.toUpperCase();
+        float sub=10;
+        float discount=5;
+        double ship= 4.99;
+        if(code.equalsIgnoreCase(discode) && app.equals("TESTING")){
+            System.out.println("a True");
+        }else{
+            System.out.println("a False");
+        }
+        if(code.equals(discode) && (sub-discount+ship)==9.99){
+            System.out.println("b True");
+        }else{
+            System.out.println("b False");
+        }
+
+    }
+
+
+}
